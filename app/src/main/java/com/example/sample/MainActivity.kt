767fa3f498b98d3e5c21dfc967912ba4711d776e
hilott <hilott.g@gmail.com>
2020-09-17 22:17:58 +0900

    add firebase

--- app/src/main/java/com/example/sample/MainActivity.kt
+++ app/src/main/java/com/example/sample/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.FirebaseApp
+import com.google.firebase.analytics.FirebaseAnalytics
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        FirebaseAnalytics.getInstance(this)
     }
 }
